9bdbaf9112e10ddb336af4fa63a9fa1cd0b1733d
mdolbear <mdolbear@gmail.com>
2023-06-03 14:24:00 -0600

    Setup suggested logging for jpa in development.

--- src/main/java/com/mjdsoftware/logbook/config/AsyncConfig.java
+++ src/main/java/com/mjdsoftware/logbook/config/AsyncConfig.java
@@ -3,10 +3,12 @@ package com.mjdsoftware.logbook.config;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
 
 import java.util.concurrent.Executor;
@@ -41,5 +43,26 @@ public class AsyncConfig {
         return new DelegatingSecurityContextAsyncTaskExecutor(tempExecutor);
 
     }
+
+
+    /**
+     * Setup SecurityContext so that the Authentication can be inherited by
+     * Threads created via Springboot that use the @Async annotation.
+     * Note that this could also be done via properties. @see spring.security.strategy
+     *
+     * @return InitializingBean
+     */
+//    @Bean
+//    public InitializingBean initializingBean() {
+// This currently causes org.springframework.security.authentication.AuthenticationCredentialsNotFoundException:
+//  An Authentication object was not found in the SecurityContext , when I run my tests. Need to investigate further
+//  The property in my yaml didn't blow up tests but not sure if it really worked. I will need to test this again
+//  with a real @Async method in the future.
+//        return () -> SecurityContextHolder.setStrategyName(
+//                SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+//
+//    }
+
+
 
 }
